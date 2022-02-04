b64d07ae41ea3c0c5146fb12505bfa7c52ad4eec
onepunchboy <49877596+qinzhouhit@users.noreply.github.com>
2022-02-04 13:39:21 -0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,16 +3,18 @@ import os
 import sys
 import collections
 
+
 def city_poi_extract(argv):
     # TODO: have POI formatted into pickle
-    '''
+    """
     file header: 0)address, 1)adname, 2)page_publish_time, 3)adcode, 4)pname,
     5)cityname, 6)name, 7)location, 8)_id, 9)type,
     e.g., 0)丰城市,1)丰城市,2)2017-06-19 02:26:39,3)360981,4)江西省,5)宜春市,
     6)怡丰花园15号楼,7)115.768309，28.152627,8)j#20170619#6d2ee48230dd6861879e5ac8c8434933,
     9)地名地址信息;门牌信息;楼栋号,
     :return: k: (lng, lat), v: a list of categories
-    '''
+    """
+    
     data_path = "some_path"
     files = []
     for i in os.listdir(data_path):
@@ -39,5 +41,6 @@ def city_poi_extract(argv):
             continue
     pickle.dump(res, open(argv[1] + "_poi_pku.p", "wb"))
 
+    
 if __name__=="__main__":
     city_poi_extract(sys.argv[1:])
